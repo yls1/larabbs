ffe2799626b663be42a4f02e40b1c1da2a88f35c
a <a@qq.com>
2020-06-10 07:03:14 +0000

    移动User模型到app\Models 目录”

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Modles\User::class,
         ],
 
         // 'users' => [
