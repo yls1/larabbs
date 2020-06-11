cbb9634e07698d71bdde3d706749d6b1ab3c1c81
a <a@qq.com>
2020-06-11 10:06:13 +0000

    翻译 URL 图标

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -12,6 +13,11 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
+
+        if(!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
