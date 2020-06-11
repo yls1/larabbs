c10d4a5d7c9d15d63c8d90fb70398766abf2d501
a <a@qq.com>
2020-06-11 00:59:59 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
