1b4a89d50a72cb6f1f326a9735ea984a78141d6d
a <a@qq.com>
2020-06-12 13:53:08 +0000

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,14 +8,17 @@ use App\Models\Topic;
 
 use App\Models\Category;
 
+use App\Models\User;
+
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic,User $user)
     {
         $topics = $topic->withOrder($request->order)
                         ->where('category_id', $category->id)
                         ->paginate(20);
+        $active_users = $user->getActiveUsers();
 
-        return view('topics.index', compact('topics', 'category'));
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
