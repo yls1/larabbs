2880012de0f76f2332d5446008d0f574ee0f3b3e
a <a@qq.com>
2020-06-12 23:45:40 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user) 
+    {
+        if(empty($user->avatar)) {
+            $user->avatar = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592015485145&di=db84dc6ad5ff2046636675a60d52f381&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D1786937383%2C3661854611%26fm%3D214%26gp%3D0.jpg';
+        }
+    }
 }
